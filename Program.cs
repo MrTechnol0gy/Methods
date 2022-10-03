d794b751673481eda76dcaa3e77d8f22f1fcbedd
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-10-03 13:37:59 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,52 +12,60 @@ namespace Methods
         static int enemyScoreValue;
         static int playerHealth;
         static int enemyDamage;
+        static int combomultiplier;
         static void Main(string[] args)
         {
             score = 0;
             enemyScoreValue = 100;
             playerHealth = 100;
             enemyDamage = 25;
+            combomultiplier = 1;
             
             Console.WriteLine("Methods.");
 
-            ShowHud();
+            while (playerHealth > 0)
+            {
+                ShowHud();
+                PlayerChoice();        
+            }
 
-            Console.WriteLine("Press any key to quit.");
-            Console.ReadKey(true);
         }
 
         static void ShowHud()
         {
-            PlayerChoice();
             Console.WriteLine("===========================");
             Console.WriteLine("Score: " + score);
+            Console.WriteLine("Player Health: " + playerHealth);
             Console.WriteLine("===========================");
-            Console.WriteLine();
-            score = score + enemyScoreValue;
-            Console.WriteLine("===========================");
-            Console.WriteLine("Score: " + score);
-            Console.WriteLine("===========================");
+            Console.WriteLine();            
         }
 
         static void PlayerChoice()
         {
-
-            Console.WriteLine("Press 'F' to shoot, or Press 'J' to get shot!");
-            Console.WriteLine("Press any other key to exit the game.");
+            
+            Console.WriteLine("Press 'F' to shoot, or Press 'J' to get shot!");                        
             if (Console.ReadKey().Key == ConsoleKey.F)
             {
-                Console.WriteLine("You've shot the enemy!");                
+                Console.WriteLine();
+                Console.WriteLine("You've shot the enemy!");
+                combomultiplier++;
+                score = score + enemyScoreValue * combomultiplier;
+                return;
             }
             else if (Console.ReadKey().Key == ConsoleKey.J)
             {
+                Console.WriteLine();
                 Console.WriteLine("You've been shot!");
-            }    
-            else
-            {
-                Console.ReadKey(true);
+                combomultiplier = 1;
+                playerHealth = playerHealth - enemyDamage;                
+                if (playerHealth <= 0)
+                {
+                    Console.WriteLine("You have died!");
+                    return;
+                }                
+            return;
             }
-
+        return;
         }
     }
 }
